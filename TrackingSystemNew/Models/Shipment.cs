8610a659dd2364dcffb9428743fada09c70cb0a7
EbtihalAlmutairi <Ebtihal.1252@gmail.com>
2021-03-22 15:17:53 +0300

    test

---
+++ TrackingSystemNew/Models/Shipment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrackingSystemNew.Models
+{
+    public class Shipment
+    {
+        [Key]
+        public int ShipmentNo { get; set; }
+        public string ShipmentStatus { get; set; }
+    }
+}
